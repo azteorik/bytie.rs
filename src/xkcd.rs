2f7dd6ee23b7e9411758c136426f703a6389a801
Onur Akdemir <onur.akdemir@icloud.com>
2024-08-30 00:34:35 +0300

    show XKCD title instead of id

--- src/xkcd.rs
+++ src/xkcd.rs
@@ -3,31 +3,27 @@ use poise::serenity_prelude as serenity;
 use reqwest;
 use serde_json::Value;
 
+struct XKCDComic {
+    title: String,
+    image_url: String,
+}
+
 /// Responds with the XKCD comic with the given ID
 #[poise::command(slash_command)]
 pub async fn xkcd(ctx: Context<'_>, #[description = "Comic ID"] id: u32) -> Result<(), Error> {
     // Fetch the XKCD image URL
-    let image_url = match fetch_xkcd_image_url(id).await {
-        Ok(url) => url,
-        Err(err) => {
-            ctx.say(format!("Failed to fetch XKCD comic: {}", err))
-                .await?;
-            return Ok(());
-        }
-    };
+    let xkcd = fetch_xkcd(id).await?;
 
-    let reply = poise::CreateReply::default()
-        .content(format!("XKCD Comic #{}", id))
-        .embed(
-            serenity::CreateEmbed::new()
-                .title(format!("XKCD Comic #{}", id))
-                .image(image_url),
-        );
+    let reply = poise::CreateReply::default().content(String::new()).embed(
+        serenity::CreateEmbed::new()
+            .title(&xkcd.title)
+            .image(xkcd.image_url),
+    );
     ctx.send(reply).await?;
     Ok(())
 }
 
-async fn fetch_xkcd_image_url(id: u32) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+async fn fetch_xkcd(id: u32) -> Result<XKCDComic, Box<dyn std::error::Error + Send + Sync>> {
     let url = format!("https://xkcd.com/{}/info.0.json", id);
     let response = reqwest::get(&url).await?;
     if !response.status().is_success() {
@@ -40,5 +36,6 @@ async fn fetch_xkcd_image_url(id: u32) -> Result<String, Box<dyn std::error::Err
         .as_str()
         .ok_or("Image URL not found")?
         .to_string();
-    Ok(image_url)
+    let title = data["title"].as_str().ok_or("Title not found")?.to_string();
+    Ok(XKCDComic { title, image_url })
 }
